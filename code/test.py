625ac5368875782b482c18c842398e8c34ebaf6e
ErikLambrechts <lambrechtserik@hotmail.com>
2018-06-25 03:10:14 +0200

    improve ments

--- code/test.py
+++ code/test.py
@@ -1,38 +1,87 @@
 import numpy as np
 
+def render_char_matrix(m):
+    print('\n'.join(["".join(r) for r in m]))
 
-class tile(object):
-    def __init__(self, value = 0):
-        self.value = value
+class Tile(object):
+    def __init__(self):
+        pass
 
     def render(self):
+        pass
         if self.value == 0:
             return " "
         return self.value
 
-class grid(object):
+class Wall(Tile):
+    def render(self):
+        return "#"
+
+class Floor(Tile):
+    def __init__(self, value = 0):
+        self.value = value
+
+    def render(self):
+        return " "
+
+class Grid(object):
     def __init__(self, n_rows, n_columns):
         self.n_rows = n_rows
         self.n_columns = n_columns
 
         self.tiles = []
-        for _ in range(n_rows):
+        for i in range(n_rows):
             row = []
-            for _ in range(n_columns):
+            for j in range(n_columns):
                 value = np.random.randint(5)
-                row.append(tile(value))
-
+                if i == 0 or j == 0:
+                    row.append(Wall())
+                elif i == n_rows - 1 or j == n_columns - 1:
+                    row.append(Wall())
+                # elif np.random.randint(8) == 0:
+                #     row.append(Wall())
+                else:
+                    row.append(Floor(value))
             self.tiles.append(row)
 
+    def render_list(self):
+        output = []
+        for r_ in self.tiles:
+            output.append([t_.render() for t_ in r_])
+        return output
 
     def render(self):
-        for r_ in self.tiles:
-            for t_ in r_:
-                print(t_.render(), end="")
-            print("")
+        render_char_matrix(self.render_list())
+
+class Player(object):
+    def __init__(self, x = 1, y = 1):
+        self.x = x
+        self.y = y
+
+    def render(self):
+        return "p"
+
+class World(object):
+    def __init__(self, grid, player = None):
+        if player == None:
+            player = Player()
+
+        self.grid = grid
+        self.player = player
+
+    def render(self):
+        out = self.grid.render_list()
+
+        player_x = self.player.x
+        player_y = self.player.y
+
+        out[player_y][player_x] = self.player.render()
+
+        render_char_matrix(out)
+
+g = Grid(5, 6)
 
 
-g = grid(3, 4)
-
-t = g.tiles[0][0]
-g.render()
+w = World(g)
+w.render()
+p = w.player
